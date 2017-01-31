ba4cde4534d33385f1dcf191def31e71214b9c70
YolandaHe1 <pro@erics-mbp.risd.edu>
2017-01-31 08:46:43 -0500

    1

--- yolandahe/project3/home.js
+++ yolandahe/project3/home.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $('.img').click(function() {
         $('.img').effect('explode');
     });
+
+    $('#main').masonry({
+        itemSelector: '.box',
+        // use element for option
+        columnWidth: '.box',
+        percentPosition: true
+      });
+
 });
